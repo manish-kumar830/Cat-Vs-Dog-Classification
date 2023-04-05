5feeb4354c48fdc10e180ace9762dea05ee12634
Manish Kumar <67259905+manish-kumar830@users.noreply.github.com>
2023-04-05 05:37:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import numpy as np
+import tensorflow_hub as hub
+# from keras.models import load_model
+from tensorflow.keras.models import load_model
+import cv2
+
+
+# Model saved with Keras model.save()
+MODEL_PATH = 'models/model.h5'
+
+# Load your trained model
+@st.cache_resource
+def loading_model():
+    model = load_model((MODEL_PATH),custom_objects={'KerasLayer':hub.KerasLayer})
+    return model
+
+model = loading_model()
+
+
+
+def model_predict(image_path, model):
+    input_image = cv2.imread(image_path)
+
+    input_image_resize = cv2.resize(input_image, (224,224))
+
+    input_image_scaled = input_image_resize/255
+
+    image_reshaped = np.reshape(input_image_scaled, [1,224,224,3])
+
+    input_prediction = model.predict(image_reshaped)
+
+    input_pred_label = np.argmax(input_prediction)
+
+    return input_pred_label
+
+def main():
+    st.title("Image Classifier")
+    file = st.file_uploader("Upload Image",type=['jpg','png','jpeg'])
+    if file is not None:
+        st.image(file,use_column_width=True)
+        save_image_path = 'uploads/'+file.name
+        with open(save_image_path,'wb') as f:
+            f.write(file.getbuffer())
+
+        if st.button("predict"):
+            result = model_predict(save_image_path,model)
+            if result==0:
+                st.error("Prediction : Cat")
+            else:
+                st.error("Prediction : Dog")
+    
+
+
+if __name__ == '__main__':
+    main()
+    
