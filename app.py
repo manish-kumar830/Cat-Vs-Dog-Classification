9863f77587fcace6fb42817c117cd6b1cb34f6d4
Manish Kumar <67259905+manish-kumar830@users.noreply.github.com>
2023-04-06 05:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import numpy as np
+from PIL import Image
 import tensorflow_hub as hub
 # from keras.models import load_model
 from tensorflow.keras.models import load_model
